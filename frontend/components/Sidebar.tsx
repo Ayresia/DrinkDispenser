8a895cc886123130ba72c624b017950069b82169
Ayresia <nalizyoncatania0@gmail.com>
2022-04-07 17:54:43 +0200

    feat(frontend): add sidebar button hover

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -1,6 +1,16 @@
 import SidebarButton from "./SidebarButton";
+import DashboardLogo from "../public/images/dashboard.svg";
+import DrinksLogo from "../public/images/drinks.svg";
+import SettingsLogo from "../public/images/settings.svg";
+import { useEffect, useState } from "react";
 
 export default function Sidebar() {
+    const [page, setPage] = useState<string>("");
+
+    useEffect(() => {
+        setPage(window.__NEXT_DATA__.page)
+    }, [])
+
     return (
         <aside className="flex flex-col w-fit gap-10 border-r-2 border-r-white border-opacity-[11%] p-10 h-full">
             <img 
@@ -8,9 +18,9 @@ export default function Sidebar() {
                 alt="Drink Dispenser Logo"
                 className="h-16"
             />
-            <SidebarButton name="Dashboard" href="/" />
-            <SidebarButton name="Drinks" href="/drinks" />
-            <SidebarButton name="Settings" href="/settings" />
+            <SidebarButton name="Dashboard" logo={DashboardLogo} page={page} href="/" />
+            <SidebarButton name="Drinks" logo={DrinksLogo} page={page} href="/drinks" />
+            <SidebarButton name="Settings" logo={SettingsLogo} page={page} href="/settings" />
         </aside>
     );
 }
--- frontend/components/SidebarButton.tsx
+++ frontend/components/SidebarButton.tsx
@@ -1,20 +1,17 @@
 export interface SidebarButtonProps {
     name: string,
     href: string,
+    page: string,
+    logo: any,
 }
 
 export default function SidebarButton(props: SidebarButtonProps) {
-    let iconNameLower = props.name.toLowerCase()
-
     return (
         <a 
             href={props.href}
-            className="flex gap-4 items-center text-white font-medium"
+            className={`flex gap-4 items-center ${props.page === props.href ? "sidebar-button" : "sidebar-button-inactive transition duration-200 ease-in-out"}`}
         >
-            <img
-                src={`images/${iconNameLower}.svg`}
-                alt={`${props.name} icon`}
-            />
+            <props.logo className="button-icon" />
             {props.name}
         </a>
     );
