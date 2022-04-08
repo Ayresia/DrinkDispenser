24248ce746ae0cd707b6bcec4ff9adc83a002d7e
Ayresia <nalizyoncatania0@gmail.com>
2022-04-08 10:33:34 +0200

    fix(frontend): chart entry inline bar

--- frontend/components/ChartEntry.tsx
+++ frontend/components/ChartEntry.tsx
@@ -6,7 +6,7 @@ export interface ChartEntryProps {
 export default function ChartEntry(props: ChartEntryProps) {
     return (
         <div className="flex gap-5 w-full items-center justify-between sm:justify-start">
-            <p className="font-semibold lg:text-lg">{props.name}</p>
+            <p className="font-semibold lg:text-lg min-w-[95px]">{props.name}</p>
             <div
                 className="hidden sm:block before:content-none bg-white h-2 rounded-md drop-shadow-[0_0px_2px_rgba(255,255,255,0.4)]"
                 style={{ width: `${props.value}%` }}
