b024e1399bc8d0f5ae2a89fe9abe1e56d46a6461
Ayresia <nalizyoncatania0@gmail.com>
2022-04-12 12:01:53 +0200

    fix(frontend): card button responsiveness

--- frontend/components/CardButton.tsx
+++ frontend/components/CardButton.tsx
@@ -11,8 +11,11 @@ export default function CardButton(props: CardButtonProps) {
     return (
         <button className={`
             flex
-            flex-col
-            justify-center
+            lg:flex-col
+            items-center
+            lg:justify-center
+            pl-5
+            lg:pl-0
             rounded-[20px]
             gap-2
             border-[1px]
@@ -20,27 +23,28 @@ export default function CardButton(props: CardButtonProps) {
             border-opacity-50
             bg-white
             bg-opacity-20
+            h-16
+            lg:h-auto
             min-w-[210px]
             relative
             `}
             onClick={props.onClick}
         >
-            { props.indicator &&
-                <div className="
-                    absolute
-                    top-4
-                    left-4
-                    w-[12px]
-                    h-[12px]
-                    before:content-['']
-                    bg-[#4CC896]
-                    rounded-lg
-                    shadow-[0_0_15px_#4CC896]
-                    "
-                />
-            }
-            <div className="flex flex-col gap-3 pt-3">
-                <img src={`./images/${props.image}.svg`} className="max-h-[25px]" />
+            <div className={`
+                block
+                lg:absolute
+                lg:top-4
+                lg:left-4
+                w-[12px]
+                h-[12px]
+                before:content-['']
+                ${ props.indicator && "bg-[#4CC896]" }
+                rounded-lg
+                shadow-[0_0_15px_#4CC896]
+                `}
+            />
+            <div className="flex flex-col gap-3 pt-0 lg:pt-3">
+                <img src={`./images/${props.image}.svg`} className="hidden lg:block max-h-[25px]" />
                 <p className="font-semibold">{props.title}</p>
             </div>
         </button>
