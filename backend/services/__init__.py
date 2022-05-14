d9b91da00594b4d6b37e3d0db90524d944115e08
Ayresia <nalizyoncatania0@gmail.com>
2022-05-14 20:09:42 +0200

    feat(frontend): email/push notifications support

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -1,17 +1,20 @@
 from config import config
-from . import simplepush
+from . import simplepush, gmail
+
+gmailClient = gmail.Client()
 
 def sendNotification(title, message, event):
     notificationType = config["notificationType"]
+    emailTo = config["email"]
 
-    match notificationType:
-        case 1: 
-            simplepush.send_notification(title, message, event)
-            # TODO: Send email alerts
-            return
-        case 2:
-            # TODO: Send email alerts
-            return
-        case 3:
-            simplepush.send_notification(title, message, event)
-            return
+    try:
+        match notificationType:
+            case 1: 
+                simplepush.send_notification(title, message, event)
+                gmailClient.sendEmail(emailTo, title, message)
+            case 2:
+                gmailClient.sendEmail(emailTo, title, message)
+            case 3:
+                simplepush.send_notification(title, message, event)
+    except Exception as e:
+        print("Unable to send notification - ", e)
---
+++ backend/services/gmail.py
@@ -0,0 +1,57 @@
+import os
+import base64
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from email.mime.text import MIMEText
+
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.send",
+    "https://www.googleapis.com/auth/gmail.compose"
+]
+
+class Client:
+    def __init__(self):
+        self.creds = None
+        self.service = None
+
+        self._getCredentials()
+        self._getService()
+
+    def _getService(self):
+        self.service = self.service = build("gmail", "v1", credentials= self.creds)
+
+    def _getCredentials(self):
+        if os.path.exists("token.json"):
+            self.creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+            
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                if not os.path.exists("credentials.json"):
+                    print("Unable to find credentials.json, email alerts are going to be disabled")
+                    return
+
+                flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+                self.creds = flow.run_local_server(port= 0)
+
+                with open("token.json","w") as token:
+                    token.write(self.creds.to_json())
+
+    def sendEmail(self, email, subject, rawMessage):
+        if not self.creds:
+            return
+
+        message = MIMEText(rawMessage)
+
+        message["to"] = email
+        message["subject"] = subject
+
+        decodedMessage = base64.urlsafe_b64encode(message.as_bytes())
+        decodedMessage = decodedMessage.decode()
+        body = { "raw": decodedMessage }
+
+        self.service.users().messages().send(userId = "me", body = body).execute()
